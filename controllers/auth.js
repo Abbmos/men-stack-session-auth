2f6b2522aab6adb7eea854beae433f75dc0cdc49
Abbas <abboshuss@gmail.com>
2025-02-03 15:38:38 +0300

    compeletd template mostly

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,29 +1,109 @@
 const User = require('../models/user')
+const bcrypt = require("bcrypt");
 
 const signUp = (req, res) => {
-
     res.render('auth/sign-up.ejs', { title: "Sign Up",msg:"test" });
-
-
+    
 }
-
-
-
 const addUser = async (req, res) => {
-
-
-    console.log(console.log('request body', req.body));
-    const userInDatabase = await User.findOne({ username: req.body.username })
-
+    console.log('request body: ', req.body)
+    const userInDatabase = await User.findOne({ username: req.body.username})
     if (userInDatabase) {
+        return res.render('auth/sign-up.ejs',{
+            title: 'Sign up', 
+            msg: 'Username already taken.'
 
-        return res.send('Username already taken.')
+
+        })
     }
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.render('auth/sign-up.ejs', {
+            title: 'Sign up',
+            msg: 'Password and Confirm Password must match.'
+        })
+    }
+    const hashedPassword =bcrypt.hashSync(req.body.password,10);
+    req.body.password=hashedPassword
     const user = await User.create(req.body)
-    console.log("new user: ", user)
-    user.send();
+   
+    req.session.user={
+        username: user.username,
+        
+    }
+    req.session.save(()=>{
+    res.redirect('/');
+
+    })
+    
+
 }
+
+
+const signInForm = (req,res)=>{
+
+res.render('auth/sign-in.ejs',{
+    title:'Sign in',
+    msg:'',
+})
+
+
+}
+
+
+const signIn = async (req,res)=>{
+    console.log("req body is ",req.body);
+    const userInDatabase = await User.findOne({username:req.body.username})    
+    if(!userInDatabase){
+       return res.render('auth/sign-in.ejs',{
+
+         title:'Sign in',
+         msg:"invalid credentials ",   
+       })
+        
+    }    
+const validPassword = bcrypt.compareSync(
+    req.body.password,
+    userInDatabase.password
+)
+if(!validPassword){
+
+    return res.render('auth/sign-in.ejs',{
+        title:'Sign in',
+        msg:'Invalid credentials Pls try again',
+    })
+}
+
+        req.session.user={
+            username: userInDatabase.username,
+            
+        }
+        req.session.save(()=>{
+        res.redirect('/');
+
+        })
+        
+
+    }
+
+
+
+
+
+    const signOut=(req,res)=>{
+        req.session.destroy(()=>{
+            res.clearCookie('connect.sid');
+             res.redirect('/');
+        });
+       
+
+
+    } 
+
+
 module.exports = {
     signUp,
     addUser,
+    signInForm,
+    signIn,
+    signOut,
 }
---
+++ controllers/vip.js
@@ -0,0 +1,10 @@
+const welcome = (req, res) => {
+   
+        res.send(`Welcome to the party ${req.session.user.username}`)
+  
+
+}
+
+module.exports={
+    welcome,
+}
